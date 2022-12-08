a8de33c1328a60b055be36a095ec717ed97698ff
hadeel <hadeel.alturaier@hotmail.com>
2022-12-08 23:16:29 +0300

    Initial Commit

---
+++ questionpage/questionpageApp.swift
@@ -0,0 +1,17 @@
+//
+//  questionpageApp.swift
+//  questionpage
+//
+//  Created by Hadeel Alturaier on 14/05/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct questionpageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
